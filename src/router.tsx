1008907270c987d0b61ab99a997d2f368a516ce4
Abdur Raheem <AbdurRaheemshaikh155@gmail.com>
2026-05-18 01:25:37 +0500

    fixes

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
+import "./styles.css";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
@@ -14,3 +16,12 @@ export const getRouter = () => {
 
   return router;
 };
+
+const router = getRouter();
+if (typeof document !== "undefined") {
+  const rootElement = document.getElementById("root");
+
+  if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+  }
+}
