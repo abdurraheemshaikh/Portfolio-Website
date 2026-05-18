c3730190bd67128d34df629acad57a07f3afdb51
Abdur Raheem Shaikh <114747533+abdurraheemshaikh@users.noreply.github.com>
2026-05-18 14:28:35 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  vite: {},
 });
